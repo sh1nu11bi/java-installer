fa7f182ad627e9d8c736cf476e3edbf9cf6b59ae
redpois0n <red@cmail.nu>
2015-02-26 19:35:03 +0100

    Latest Java (x86) link set

--- src/main.c
+++ src/main.c
@@ -10,18 +10,19 @@ int main(void) {
     CURL *curl;
     FILE *fp;
     CURLcode res;
-    char *url = "";
+    char *url = "http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jre-8u31-windows-i586.exe";
     char outfilename[FILENAME_MAX] = "jre.exe";
 
     curl = curl_easy_init();
 
     if (curl) {
-        fp = fopen(outfilename,"wb");
+        fp = fopen(outfilename, "wb");
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
         fclose(fp);
