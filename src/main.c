ae10789eda810eab94774d980fbb7e3b8cbb7119
redpois0n <red@cmail.nu>
2015-02-26 20:39:06 +0100

    Checks if system is 64 bit and downloads correct JRE

--- src/main.c
+++ src/main.c
@@ -1,35 +1,61 @@
+#include <windows.h>
+#include <tchar.h>
 #include <stdio.h>
 #include <curl/curl.h>
 
+typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS)(HANDLE, PBOOL);
+
+LPFN_ISWOW64PROCESS fnIsWow64Process;
+
+BOOL IsWow64() {
+	BOOL bIsWow64 = FALSE;
+
+	fnIsWow64Process = (LPFN_ISWOW64PROCESS) GetProcAddress(GetModuleHandle(TEXT("kernel32")), "IsWow64Process");
+
+	if (NULL != fnIsWow64Process) {
+		if (!fnIsWow64Process(GetCurrentProcess(), &bIsWow64)) {
+			return FALSE;
+		}
+	}
+
+	return bIsWow64;
+}
+
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    size_t w = fwrite(ptr, size, nmemb, stream);
-    return w;
+	size_t w = fwrite(ptr, size, nmemb, stream);
+	return w;
 }
 
 int main(void) {
-    CURL *curl;
-    FILE *fp;
-    char *url = "http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jre-8u31-windows-i586.exe";
-    char outfilename[FILENAME_MAX] = "jre.exe";
+	CURL *curl;
+	FILE *fp;
+	char *url;
+	char outfilename[FILENAME_MAX] = "jre.exe";
 
-    struct curl_slist *cookies = curl_slist_append(NULL, "Cookie: oraclelicense=accept-securebackup-cookie");
-    curl = curl_easy_init();
+	if (IsWow64()) {
+		url = "http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jre-8u31-windows-x64.exe";
+	} else {
+		url = "http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jre-8u31-windows-i586.exe";
+	}
 
-    if (curl) {
-        fp = fopen(outfilename, "wb");
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, cookies);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	struct curl_slist *cookies = curl_slist_append(NULL, "Cookie: oraclelicense=accept-securebackup-cookie");
+	curl = curl_easy_init();
 
-        curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-        fclose(fp);
+	if (curl) {
+		fp = fopen(outfilename, "wb");
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, cookies);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-        system("jre.exe");
-    }
-    return 0;
+		curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		fclose(fp);
+
+		system("jre.exe");
+	}
+	return 0;
 }
